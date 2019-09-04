2fb04e13f7616c64abcfaba558be8d64e742818c
Diego Hurtado <diegohurtado@gmail.com>
2019-09-04 00:20:40 -0600

    chg: dev: Report errors

--- lib/opentelemetry_python_autoagent/initialize/sitecustomize.py
+++ lib/opentelemetry_python_autoagent/initialize/sitecustomize.py
@@ -1,11 +1,15 @@
 from pkg_resources import iter_entry_points
 
-# FIXME avoid a hard dependency on a client like this one, create a
-# plugin-based interface to handle this
-
 
 for entry_point in iter_entry_points(
     group='opentelemetry_python_autoagent_10'
 ):
 
-    entry_point.load()().monkeypatch()
+    try:
+
+        entry_point.load()().monkeypatch()
+
+    except Exception as error:
+
+        print(entry_point.name)
+        print(error)
